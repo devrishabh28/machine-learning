cea8c718af9b8733749a77f0c9c7ad8fbb30b0e6
Rishabh Dev <devrishabh28@gmail.com>
2022-12-30 16:25:28 +0530

    added adaptive gradient optimizer

--- neural-networks/learning-from-scratch/optimizers.py
+++ neural-networks/learning-from-scratch/optimizers.py
@@ -55,3 +55,45 @@ class StochasticGradientDescent:
 
         #  Post-Update Parameters
         self.iterations += 1
+
+
+#  Adaptive Gradient Optimizer
+class AdaptiveGradient:
+
+    #  Initialize optimizer
+    def __init__(self, learning_rate=1, decay=0., epsilon=1e-7) -> None:
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+
+    #  Update parameters
+    def updateParameters(self, layers):
+
+        #  Pre-Update Parameters
+        if self.decay:
+            self.current_learning_rate = self.learning_rate / \
+                (1 + self.decay * self.iterations)
+
+        #  Update Parameters
+        for layer in layers:
+            if not hasattr(layer, 'weight_cache'):
+                #  If layer doesn't contain cache arrays,
+                #  create them filled with zeros.
+                layer.weight_cache = np.zeros_like(layer.weights)
+                layer.bias_cache = np.zeros_like(layer.biases)
+
+            #  Update cache with squared current gradients
+            layer.weight_cache += layer.dweights**2
+            layer.bias_cache += layer.dbiases**2
+
+            #  Vanilla SGD parameter update with
+            #  normalization using square rooted cache
+            layer.weights += -self.current_learning_rate * \
+                layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+            layer.biases += -self.current_learning_rate * \
+                layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+        #  Post-Update Parameters
+        self.iterations += 1
--- neural-networks/learning-from-scratch/train_adagrad.py
+++ neural-networks/learning-from-scratch/train_adagrad.py
@@ -1,14 +1,14 @@
-#  Training nerual network using Stochastic Gradient Descent.
+#  Training neural network using Adaptive Gradient.
 import numpy as np
-from nnfs.datasets import spiral_data
+import optimizers
 import layer
 import activation_functions as af
-import optimizers
+from nnfs.datasets import spiral_data
 
 #  Create dataset
 X, y = spiral_data(samples=100, classes=3)
 
-#  Create a dense layer with 2 input features and 6 output features.
+#  Create a dense layer with 2 input features and 64 output values.
 dense1 = layer.LayerDense(2, 64)
 
 #  Create ReLU activation
@@ -17,11 +17,11 @@ activation1 = af.ActivationReLU()
 #  Create second dense layer with 64 input features and 3 output values.
 dense2 = layer.LayerDense(64, 3)
 
-#  Create Softmax Classifier's combined loss and activation.
+#  Create Softmax Classifier's combined loss and activation
 loss_activation = af.SoftmaxClassifier()
 
 #  Create optimizer
-optimizer = optimizers.StochasticGradientDescent(decay=1e-3, momentum=0.9)
+optimizer = optimizers.AdaptiveGradient(decay=1e-4)
 
 #  Train in loop
 for epoch in range(10001):
@@ -34,15 +34,15 @@ for epoch in range(10001):
     activation1.forward(dense1.output)
 
     #  Perform a forward pass through the second dense layer
-    #  takes the output of the activation function as input.
+    #  takes the output of activation function of the first layer as iputs.
     dense2.forward(activation1.output)
 
     #  Perform a forward pass through the activation/loss function
     #  takes the output of the second dense layer as input and returns loss.
     loss = loss_activation.forward(dense2.output, y)
 
-    #  Calculate accuracy from the output of activation2 and targets
-    #  calculate values along first axis.
+    #  Calculate accuracy from output of activation2 and targets
+    #  caculate values along first axis
     predictions = np.argmax(loss_activation.output, axis=1)
     if len(y.shape) == 2:
         y = np.argmax(y, axis=1)
@@ -53,7 +53,7 @@ for epoch in range(10001):
             f'epoch: {epoch}, acc: {accuracy:.3f}, loss: {loss:.2f}, lr: {optimizer.current_learning_rate}'
         )
 
-    #  Back Propagation
+    #  Backward Pass
     loss_activation.backward(loss_activation.output, y)
     dense2.backward(loss_activation.dinputs)
     activation1.backward(dense2.dinputs)
