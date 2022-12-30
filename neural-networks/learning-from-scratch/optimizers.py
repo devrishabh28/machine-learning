e50c7e498c793f742a6426f04b51a7f1afb97d6a
Rishabh Dev <devrishabh28@gmail.com>
2022-12-30 17:25:12 +0530

    added root mean square propagation optimizer

--- neural-networks/learning-from-scratch/optimizers.py
+++ neural-networks/learning-from-scratch/optimizers.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 
-#  Stochastic Gradient Descent Optimizer
+#  Stochastic Gradient Descent Optimizer (SGD)
 class StochasticGradientDescent:
 
     #  Initialize optimizer
@@ -57,7 +57,7 @@ class StochasticGradientDescent:
         self.iterations += 1
 
 
-#  Adaptive Gradient Optimizer
+#  Adaptive Gradient Optimizer (AdaGrad)
 class AdaptiveGradient:
 
     #  Initialize optimizer
@@ -80,13 +80,58 @@ class AdaptiveGradient:
         for layer in layers:
             if not hasattr(layer, 'weight_cache'):
                 #  If layer doesn't contain cache arrays,
-                #  create them filled with zeros.
+                #  create them filled with zeroes.
                 layer.weight_cache = np.zeros_like(layer.weights)
                 layer.bias_cache = np.zeros_like(layer.biases)
 
             #  Update cache with squared current gradients
             layer.weight_cache += layer.dweights**2
             layer.bias_cache += layer.dbiases**2
+
+            #  Vanilla SGD parameter update with
+            #  normalization using square rooted cache
+            layer.weights += -self.current_learning_rate * \
+                layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+            layer.biases += -self.current_learning_rate * \
+                layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+        #  Post-Update Parameters
+        self.iterations += 1
+
+
+#  Root Mean Square Propagation Optimizer (RMSProp)
+class RootMeanSquarePropagation:
+
+    #  Initialze optimizer
+    def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, rho=0.9) -> None:
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
+
+    #  Update parametrs
+    def updateParameters(self, layers):
+
+        #  Pre-Update Parameters
+        if self.decay:
+            self.current_learning_rate = self.learning_rate / \
+                (1 + self.decay * self.iterations)
+
+        #  Update Parameters
+        for layer in layers:
+            if not hasattr(layer, 'weight_cache'):
+                #  If layer doesn't contain cache arrays,
+                #  create them filled with zeroes.
+                layer.weight_cache = np.zeros_like(layer.weights)
+                layer.bias_cache = np.zeros_like(layer.biases)
+
+            #  Update cache with squared current gradients.
+            layer.weight_cache = self.rho * layer.weight_cache + \
+                (1 - self.rho) * layer.dweights**2
+            layer.bias_cache = self.rho * layer.bias_cache + \
+                (1 - self.rho) * layer.dbiases**2
 
             #  Vanilla SGD parameter update with
             #  normalization using square rooted cache
